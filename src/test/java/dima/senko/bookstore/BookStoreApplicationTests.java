1aaa0154b7fefcfbfbc3ae70e1547ff03ca785f4
Dmytro Senko <@@dmytro.senko>
2024-02-10 17:48:27 +0200

    start init

---
+++ src/test/java/dima/senko/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,15 @@
+package dima.senko.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
