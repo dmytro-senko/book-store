1e60f00b37c73ee585159ff2edb7695244c40081
Dmytro Senko <@@dmytro.senko>
2024-02-12 16:27:49 +0200

    fix: add column name & moved @Repository

--- src/main/java/dima/senko/bookstore/repositiry/impl/BookRepositoryImpl.java
+++ src/main/java/dima/senko/bookstore/repositiry/impl/BookRepositoryImpl.java
@@ -7,9 +7,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public class BookRepositoryImpl implements BookRepository {
     private final SessionFactory sessionFactory;
 
