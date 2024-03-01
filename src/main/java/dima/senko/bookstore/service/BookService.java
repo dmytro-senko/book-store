b92619e91667ff723c9eadf42d98585446df1e31
dmytro <dmytro.senko@onlyplay.net>
2024-03-01 18:21:23 +0200

    feat: add pagination, sorting, swagger

--- src/main/java/dima/senko/bookstore/service/BookService.java
+++ src/main/java/dima/senko/bookstore/service/BookService.java
@@ -4,12 +4,13 @@ import dima.senko.bookstore.dto.BookDto;
 import dima.senko.bookstore.dto.BookSearchParametersDto;
 import dima.senko.bookstore.dto.CreateBookRequestDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
 
     BookDto save(CreateBookRequestDto book);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findBookById(Long id);
 
